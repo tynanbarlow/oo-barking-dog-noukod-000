e3431fad88789e477845477aa545062b2d2dee17
Tynan Barlow <tynan@noukod.org>
2018-10-10 11:07:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog
+
+  def initialize(name_get)
+    @name = name_get
+  end
+
+  def name(name_get)
+    @name
+  end
+
+  def name=(name_get)
+    @name = name_get
+  end
+
+end
